dc84c66c38c27acc3aa6f2908940c144772908d9
billchungiii <billchungiii@gmail.com>
2018-04-11 02:45:02 +0800

    加入專案檔案。

---
+++ DrawListViewSample001/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace DrawListViewSample001
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(58, 65);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(600, 419);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(700, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(204, 71);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(700, 159);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(204, 36);
+            this.textBox1.TabIndex = 2;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(700, 238);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(142, 28);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 619);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ DrawListViewSample001/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawListViewSample001
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitialListView();
+        }
+
+
+
+        private void InitialListView()
+        {
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+            listView1.LabelEdit = false;
+            listView1.FullRowSelect = true;
+            listView1.Columns.Add("編號", 100);
+            listView1.Columns.Add("測試文字", 100);
+            for (int i = 0; i < 10; i++)
+            {
+                var item = new ListViewItem($"No.{i}");
+                item.SubItems.Add($"文字{i}");
+                listView1.Items.Add(item);
+            }
+            listView1.OwnerDraw = true;
+            listView1.DrawSubItem += ListView1_DrawSubItem;
+            listView1.DrawColumnHeader += ListView1_DrawColumnHeader;
+        }
+
+        private void ListView1_DrawColumnHeader(object sender, DrawListViewColumnHeaderEventArgs e)
+        {
+            e.DrawDefault = true;
+        }
+
+        private void ListView1_DrawSubItem(object sender, DrawListViewSubItemEventArgs e)
+        {
+            if (e.Item.Selected)
+            {
+                if (((ListView)sender).Focused)
+                {
+                    e.Graphics.FillRectangle(new SolidBrush(Color.SkyBlue), e.Bounds);
+                }
+                else
+                {
+                    e.Graphics.FillRectangle(new SolidBrush(Color.Red), e.Bounds);
+                }
+                e.Graphics.DrawString(e.Item.Text, listView1.Font, new SolidBrush(Color.White), e.Bounds.Location);
+            }
+            else
+            {
+                e.DrawBackground();
+                e.Graphics.DrawString(e.Item.Text, listView1.Font, new SolidBrush(Color.Black), e.Bounds.Location);
+            }
+        }
+    }
+}
